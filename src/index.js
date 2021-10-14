7d34112016b741f44ca719de9bbb117ae7654cc6
Carolina-Triana <wile1207@gmail.com>
2021-10-14 10:02:58 -0500

    creado ventana nodal, formulario funcionales, inicio de algoritmo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import validator from './validator.js';
+const fromCard = document.getElementById("valueCard");
+//const cardOcult = document.getElementById("cardOcult");
 
-console.log(validator);
+// eslint-disable-next-line no-console
+//console.log(cardOcult);
+
+
+//agregando un evento para que se ejecute cuando 'keyup' se levante la tecla
+fromCard.cardOcult.addEventListener( 'keyup' ,  ( e )  =>  {
+//(e)creando el evento para acceder a el, con .target que es donde se encuentra el input .value acceder al valor
+    let cardOcult = e.target.value;
+    validator.isValid(cardOcult)
+//volviendo a declarar la misma variable para agregar el metodo .replace y agregar expresiones regulares
+fromCard.cardOcult.value = cardOcult
+    .replace(/\D/g, '') //elimina las letras al levantar la tecla
+    .replace(/\s/g, '')//elimina los espacios en blanco que el usuario ingrese
+    .replace(/(.{4})/g, '$1 ') //agrega un espacio en blanco cada 4 digitos
+    .trim(); //elimina el ultimo caracter de una cadena, en este caso el ultimo espacio en blanco
+    
+}) 
+
+     
+
+const ocult = document.getElementById("CardNumber");
+const container = document.getElementById("buttonValidar");
+const visible = document.getElementById("ValidCard");
+const cerrar = document.getElementById("close");
+const modal = document.getElementById("modalValidate");
+const open = document.getElementById("modalopen");
+
+
+
+container.addEventListener('click', () => {
+  visible.style.display = "block";
+  ocult.style.display = "none"; 
+    
+})
+
+cerrar.addEventListener('click',() => {
+    modal.style.display = "none";
+})
+
+open.addEventListener('click',() =>{
+    modal.style.display = "block";
+    
+})
+ 
+
+
+
+
+
+
+
+
+
+     fromCard.addEventListener('submit', (e) => {
+         e.preventDefault()
+     })
+
+  //alert(validator.isValid());
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
-};
 
+  isValid: function () {
+    //divide el numero por digitos y lo almacena en un array de strings, con map lo transforma en array de numeros
+    let cardn = "1234567891234560";
+    alert(cardn);
+    let char = cardn.split("").map(Number);
+    //reversa el array char
+    let charReverse = char.reverse();
+    //busca las posiciones pares (index impar) del array y lo multiplica x 2
+    for (var i = 0; i < charReverse.length; i++) {
+      if (i % 2 !== 0) {
+        charReverse[i] = charReverse[i] * 2;
+        // condicional para separar (split) y sumar (reduce) los digitos convertidos en numero (map) del valor si son mayor o igual a 10
+        if (charReverse[i] >= 10) {
+          charReverse[i] = String(charReverse[i])
+            .split("")
+            .map(Number)
+            .reduce(function (a, b) {
+              let c = a+b;
+              alert(c);
+              return c;
+              
+            });
+        }
+      } else {
+        charReverse[i] = charReverse[i] * 1;
+      }
+    }
+  
+    let valid = charReverse.reduce(function (a, b) {
+      return a + b;
+    });
+
+    if (valid % 10 == 0) {
+      //tarjeta valida
+      return true;
+    } else {
+      //tarjeta invalida
+      return false;
+    }
+  }
+}
 export default validator;
